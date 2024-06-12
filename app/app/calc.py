27ce10d1d9335eeb984860a03b8817a4a4704b2e
Roei Rahamim <roeiex74@gmail.com>
2024-06-12 15:16:51 +0300

    Added postgres dependencies

---
+++ app/app/calc.py
@@ -0,0 +1,8 @@
+"""
+ Simple calculation class 
+ For test class domnstration purposes
+"""
+
+
+def add(x, y):
+    return x + y
---
+++ app/app/test.py
@@ -0,0 +1,15 @@
+from django.test import SimpleTestCase
+
+from .calc import add
+
+
+class CalcTests(SimpleTestCase):
+    """Test calc module"""
+
+    def test_add_numbers(self):
+        test_cases = [(1, 2, 3), (5, 7, 12), (-1, -1, -2), (0, 0, 0), (100, 200, 300)]
+
+        for x, y, expected in test_cases:
+            with self.subTest(x=x, y=y, expected=expected):
+                res = add(x, y)
+                self.assertEqual(res, expected)
